d8e3bdbfd2323d8cc2682807b63439a90bfdd389
Fabio Ceolin <fabioceolin98@gmail.com>
2023-08-13 16:50:45 -0500

    SignIn and SignUp

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { Text, View, StatusBar } from "react-native";
 import {
-  useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
+  useFonts,
 } from "@expo-google-fonts/roboto";
+import { NativeBaseProvider } from "native-base";
+import { StatusBar } from "react-native";
+
+import { Routes } from "./src/routes";
+
+import { Loading } from "@components/Loading";
+import { THEME } from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View style={{flex: 1, alignItems:'center', justifyContent: 'center', backgroundColor: '#202024'}}>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
-        barStyle='light-content'
-        backgroundColor='transparent'
+        barStyle="light-content"
+        backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Hello World!</Text> : <View />}
-    </View>
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
